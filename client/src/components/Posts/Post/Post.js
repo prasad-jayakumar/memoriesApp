763a616009949094c70274a19570081858ea9b37
Prasad <prasad@coras-MacBook-Pro.local>
2021-05-14 16:52:05 +0530

    first commit - build in progress

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import useStyles from './styles';
+
+const Post = () => {
+    const classes = useStyles();
+    return(
+        <h1 className={classes.card}>Post</h1>
+    );
+};
+
+export default Post;
---
+++ client/src/reducers/posts.js
@@ -0,0 +1,10 @@
+// export default reducer = (posts = [], action) => {
+//     switch (posts.value) {
+//         case 'FETCH_ALL':
+//             return posts;
+//         case 'CREATE':
+//             return posts;
+//         default:
+//             return posts;
+//     }
+// };
