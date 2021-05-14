3ba7d2acee303d3161427513c960de532534cccf
Prasad <prasad@coras-MacBook-Pro.local>
2021-05-14 18:38:33 +0530

    adding post changes

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from "redux";
-// import thunk from "react-thunk";
+import thunk from "redux-thunk";
 import App from './App';
 
-// import reducers from './reducers';
-// const store = createStore(reducers, compose(applyMiddleware(thunk)));
+import reducers from './reducers';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-    // <Provider store={store}>
-    //         <App />
-    // </Provider>,
-    <App />,
+    <Provider store={store}>
+            <App />
+    </Provider>,
     document.getElementById('root')
 );
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,10 +1,10 @@
-// export default reducer = (posts = [], action) => {
-//     switch (posts.value) {
-//         case 'FETCH_ALL':
-//             return posts;
-//         case 'CREATE':
-//             return posts;
-//         default:
-//             return posts;
-//     }
-// };
+export default (posts = [], action) => {
+    switch (action.type) {
+        case 'FETCH_ALL':
+            return action.payload;
+        case 'CREATE':
+            return posts;
+        default:
+            return posts;
+    }
+};
